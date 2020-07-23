559aeeba6981d5e08c4297687dc35b29af6ff07e
Fer <fernandoza@gmail.com>
2020-07-23 16:24:53 -0500

    finish my lecture about html + js

---
+++ js/external-example.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("External works!");
+
+// alert("External works!");
+
+// var response = confirm("Is it raining?")
+// console.log("response", response);
+
+var name = prompt("What's your name?");
+console.log(typeof name);
+console.log("length", name.length);
+console.log("name", "The name that you entered is : "+name.toUpperCase());
