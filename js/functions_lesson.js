8d1d641b37e03e9a265b5c85282f8fcade59406b
Zach Gulde <zachgulde@gmail.com>
2020-07-24 07:43:19 -0500

    Add functions lesson notes

---
+++ js/functions_lesson.js
@@ -0,0 +1,35 @@
+///////////////
+// Functions //
+///////////////
+
+// We've already been using functions
+// Vocab: function, call, invoke
+// Demo: function calls are expressions just like arithmatic operators; results are replaced with values
+
+// Demo: Defining our own function -- increment, decrement
+// Vocab: argument, parameter, body, return
+// Discussion: when will we use functions in practice?
+
+// Mini Exercise: Write a function named addTwo and test it out the same way we've used the increment function.
+//
+// - What happens if you omit the return statement?
+// - What happens if you omit the argument?
+// - What happens if you omit the parameter?
+// - What other ways can you intentionally "break" function definition and invokation code? What kind of errors do you get?
+
+// Vocab + Demo: Function expression vs declaration; anonymous functions
+
+// Demo: combine increment with user input. How many different functions are being used? How many are user-defined functions?
+// Demo: extract number function -- "$123.45" -> 123.45 ; "  $12" -> 12
+
+///////////
+// Scope //
+///////////
+
+// Vocab: global, local
+
+// Demo: local variables aren't accessible in the global scope
+// Demo: increment function with global and local variables x
+// Demo: modifying a variable (str, bool, number) inside of a function
+
+// Summary: which is better, global or local? + IIFEs
