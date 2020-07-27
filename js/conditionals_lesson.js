669b7fd36b1f4c248660908d1b2719b205403790
Zach Gulde <zachgulde@gmail.com>
2020-07-27 09:29:03 -0500

    Add conditionals lecture notes

---
+++ js/conditionals_lesson.js
@@ -0,0 +1,36 @@
+/**
+ * Conditionals
+ *
+ * A Code Execution Roadtrip
+ */
+
+/**
+ * If
+ */
+
+// if
+// analogy: stopping for gas
+// example: navbar custom name greeting
+
+// if-else
+// analogy: fork in the road
+// example: clicking on "buy"; check login status
+
+// if-else if-else
+// analogy: roundabout
+// example: click on "buy"; check login status, check if prime member
+
+/**
+ * Switch
+ */
+
+// switch -- more limited if-else chain
+// context: less common, but you should be able to read it
+// example: http status code in response
+
+/**
+ * Ternary Operator
+ */
+
+// ternary operator -- conditional expression (as opposed to statement)
+// condition ? value_if_true : value_if_false
