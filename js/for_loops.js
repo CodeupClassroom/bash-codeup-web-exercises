32a5f1b441b30c30bedf026ee8895004ae3af148
Fer <fernandoza@gmail.com>
2020-07-30 15:42:59 -0500

    feat: solves the first exercise on loops

---
+++ js/for_loops.js
@@ -0,0 +1,7 @@
+function showMultiplicationTable(aNumber){
+    for(var c = 1 ; c <= 10 ; c++){
+        console.log(aNumber+" x " + c +" = " + (aNumber * c));
+    }
+}
+
+showMultiplicationTable(7);
