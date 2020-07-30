f87ec5dff8dda1da73c424b85cc4650be8632d97
Fer <fernandoza@gmail.com>
2020-07-30 15:51:59 -0500

    feat: solves the second exercise on loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -5,3 +5,19 @@ function showMultiplicationTable(aNumber){
 }
 
 showMultiplicationTable(7);
+
+// Returns a random integer between min and max.
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+for(var i = 1 ;i <= 10 ; i++){
+
+    var randomNumber = getRandomInt(20, 200);
+
+    if(randomNumber % 2 === 0){
+        console.log(randomNumber + ' even');
+    } else {
+      console.log(randomNumber + ' odd');
+    }
+}
