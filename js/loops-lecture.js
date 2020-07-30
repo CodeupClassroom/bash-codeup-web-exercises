99da66c75833e48fa69088d5dc398aa7ba989c32
Fer <fernandoza@gmail.com>
2020-07-30 11:23:43 -0500

    feat: finished the examples for break/continue

--- js/loops-lecture.js
+++ js/loops-lecture.js
@@ -24,3 +24,37 @@ do {
     console.log(doStart);
     doStart--;
 } while( doStart >= 0)
+
+for(var x = 0 ; x <= 10 ; x++){
+    console.log('x', x);
+}
+
+// for loop with a break
+var numberToBreakAt = 5;
+
+for(var x = 0 ; x <= 10 ; x++){
+    console.log('x', x);
+    if(x === numberToBreakAt){
+        break;
+    }
+}
+
+// for loop with a break
+var employeeNumberToSkip = 3;
+var anotherEmployeeNumberToSkip = 8;
+
+for(var x = 0 ; x <= 10 ; x++){
+    if(x === employeeNumberToSkip || x === anotherEmployeeNumberToSkip){
+        continue;
+    }
+    console.log(x + " gets paid");
+}
+
+while(true){
+    var name = prompt("What's your name?");
+    if(name === ""){
+        console.log('empty name is not valid');
+        break;
+    }
+    console.log('name is valid');
+}
