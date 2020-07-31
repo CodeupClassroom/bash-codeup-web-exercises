ee89a48c75abb3961a89e074674e49cfd5410b8e
Daniel Fryar <daniel.fryar@gmail.com>
2020-07-31 10:21:59 -0500

    Finished Do While loop exercise

--- js/while.js
+++ js/while.js
@@ -4,3 +4,18 @@ while (i < 65536) {
     i += i;
     console.log(i);
 }
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+do {
+    var cones = Math.floor(Math.random() * 5) + 1;
+    // decide if we have enough
+    if (cones <= allCones) {
+        //sell some cones
+        console.log(cones + " cones sold...");
+        allCones -= cones;
+    } else {
+        console.log("Cannot sell you " + cones + " cones I only have " + allCones + "...");
+    }
+} while (allCones > 0);
+
+console.log("Yay! I sold them all!");
