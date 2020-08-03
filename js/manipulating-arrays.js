f6604d70a65b3e6ff196315f77a08a7402d4b9f2
Fer <fernandoza@gmail.com>
2020-08-03 10:09:08 -0500

    feat: reverse and sort examples

--- js/manipulating-arrays.js
+++ js/manipulating-arrays.js
@@ -15,3 +15,44 @@ console.log('after pop()', daysOfTheWeek);
 daysOfTheWeek.shift();
 
 console.log('after shift()', daysOfTheWeek);
+
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'red'];
+
+console.log('color ', colors.indexOf("blue"));
+
+colors.unshift('black');
+
+console.log(colors.indexOf("reds")); // returns -1 when element is not found
+
+console.log('color', colors.indexOf("blue"));
+
+// for(var x=0; x< colors.length ;x++){
+//     if(colors[x] === 'blue'){
+//         console.log("found fav color");
+//         break;
+//     }
+// }
+
+var slice = colors.slice(0, 3); // first param indicates the index to start with including it, second param is to indicate up to which element without including it
+console.log(colors);
+console.log(slice);
+
+// all the colors from 0 to blue including
+var moreSlices = colors.slice(0, colors.indexOf("blues") );
+console.log(moreSlices);
+
+colors.reverse();
+
+console.log(colors);
+
+colors.sort();
+
+console.log(colors);
+
+var moreColors = ["aColor", colors , 'zColor'];
+
+console.log(moreColors);
+
+
+
+
