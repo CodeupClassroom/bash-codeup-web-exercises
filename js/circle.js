d60a0f0bda5f5e8274bc012322a2460dfce8eb06
Douglas Hirsh <douglas@codeup.com>
2020-08-06 11:11:46 -0500

    feat: Math Object Exercise

---
+++ js/circle.js
@@ -0,0 +1,48 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+            var area = Math.PI * Math.pow(this.radius, 2);
+
+            return area;
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+            var area;
+            if(doRounding) {
+                area = Math.round(this.getArea());
+            } else {
+                area = this.getArea();
+            }
+
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
